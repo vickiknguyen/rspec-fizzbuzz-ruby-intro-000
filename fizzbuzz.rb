05daa0cc8c7a3c22cde22fbc60121fe2573ffbd5
Vicki Kieu Nguyen <vickikieunguyen@gmail.com>
2020-11-02 20:02:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizz (int)
+def fizzbuzz (int)
   if int % 3 == 0
     "Fizz"
-  end
-end
-
-def buzz (int)
-  if int % 5 == 0
-    "Buzz"
   end
 end
